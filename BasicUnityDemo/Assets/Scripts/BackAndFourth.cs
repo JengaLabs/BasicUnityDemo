18729c5a9dae87a3895d4855cfb1018a2953a749
Pig <jengalabscp@gmail.com>
2022-11-05 17:14:31 -0400

    Demo0.1

--- BasicUnityDemo/Assets/Scripts/BackAndFourth.cs
+++ BasicUnityDemo/Assets/Scripts/BackAndFourth.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class BackAndFourth : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    //Speed the object moves
+    public float speed = 1.0f;
+
+    //Positions the object moves between
+    public float maxZ = 1.0f;
+    public float minZ = -1.0f;
+
+    //Which direciton is the object currently moving in
+    private int _direction = 1;
+
+
+    private void Update()
     {
+        transform.Translate(0, 0, _direction * speed * Time.deltaTime);
+
+        bool bounced = false;
+        if (transform.localPosition.z >= maxZ || transform.localPosition.z <= minZ)
+        {
+            _direction = -_direction;
+            bounced = true;   
+        }
         
+
+
+        transform.Translate(0, 0, _direction * speed * Time.deltaTime);
+
+
+
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
